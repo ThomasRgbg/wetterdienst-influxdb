e0888b028575d16a84d8a77969cfaf5f37c8e65a
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2023-12-08 17:25:24 +0100

    Initial checkin with demo data

---
+++ config_data.py
@@ -0,0 +1,13 @@
+
+#
+# Device Database and registry
+#
+
+# Need to be adapted to your local devices.
+# And these values here are just default values, not my ones... ;)
+
+influxdb_url = 'http://192.168.100.222:8086'
+influxdb_token = "adfadfadfadsfsad=="
+influxdb_org = 'lalala'
+influxdb_bucket = 'lalala/autogen'
+
